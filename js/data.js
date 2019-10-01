b9eaff098119ebbcfd25f116cde8d4c92cac1d4f
Lara Farwick <larafarwick@Laras-MBP.fritz.box>
2019-10-01 10:22:07 +0200

    Added quotes

--- js/data.js
+++ js/data.js
@@ -8,5 +8,13 @@ const quotes = [
     {
         quote: "Stay hungry stay foolish",
         person: "Jobs"
+    },
+    {
+        quote: "To be, or not to be, that is the question.",
+        person: "Shakespeare"
+    },
+    {
+        quote: "All's well that ends well.",
+        person: "Shakespeare"
     }
 ]
