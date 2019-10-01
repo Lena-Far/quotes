4ce033656ab98d7fbee7ebc0f82e75be78001aa6
Arne Farwick <lenafarwick@scanin.onmicrosoft.com>
2019-10-01 09:23:11 +0200

    Initial Commit

---
+++ js/data.js
@@ -0,0 +1,12 @@
+//Try to add interesting quotes of interesting people here
+
+const quotes = [
+    {
+        quote: "In a gentle way you can shake the world",
+        person: "Gandhi"
+    },
+    {
+        quote: "Stay hungry stay foolish",
+        person: "Jobs"
+    }
+]
